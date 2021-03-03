66560e8a4258edcfa132ff0c66c6e93cd8891010
Matt Nield <mattnield@gmail.com>
2021-03-03 00:44:30 +0000

    Create simple project to retrieve content types.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Kentico.Kontent.Delivery.Builders.DeliveryClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace kontent_project_dokument.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string ProjectId{get;set;}
+        public IList<Kentico.Kontent.Delivery.Abstractions.IContentType> Types{get;set;}
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var projectId = Request.Form["ProjectId"];
+            _logger.Log(LogLevel.Information, projectId);
+
+            if(!Guid.TryParse(projectId, out Guid projectGuid)){
+                // Invalid project ID
+                return;
+            }
+
+            var client = DeliveryClientBuilder
+                .WithOptions(builder => builder
+                    .WithProjectId(projectId)
+                    .UseProductionApi()
+                    .Build())
+                    .Build();
+            
+            var typesResult = client.GetTypesAsync();
+            Types = typesResult.Result.Types;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Kontent Dokumentor
+
+The project is an experiment in createing light-weight documentation for a Kentico Kontent project by analysing the content types and creating some simple HTML output. It's crude and built on the dotnet razor pages template using `dotnet new webapp`.
+
+## Run
+
+- Restore the project from github
+- Use `dotnet run` in the terminal to start the application
+
+## Usage
+
+Enter your *Project ID* in the form on tehindex page and select *Dokument!*. The page shoudl reload with a list of known content types.
